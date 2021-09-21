5fb6be16a08915cbeb2eba21ba5bd9c86c7b5cd1
19552321 <yatsenko.yuris@gmail.com>
2021-09-21 15:37:15 +0300

    Second commit, update method numberOfCities

--- src/WorkWithList.java
+++ src/WorkWithList.java
@@ -27,32 +27,11 @@ public class WorkWithList {
     }
 
     public static void numberOfCities(List<City> cities){
-        List <Integer> counts = new ArrayList<>();
+        Map <String, List <City>> countCities= cities.stream().
+                 collect(Collectors.groupingBy(City::getDistrict, Collectors.toList()));
 
-        List <String> districtsAll = cities.stream().
-                map(City::getDistrict).
-                collect(Collectors.toList());
-        List <String> districts = districtsAll.stream().
-                distinct().
-                collect(Collectors.toList());
-
-        for (String district1: districts) {
-            int count = 0;
-            for (String district2: districtsAll) {
-                if(district1.equals(district2)){
-                    count++;
-                }
-            }
-            counts.add(count);
-        }
-
-        Map <String, Integer> countCities = new HashMap<>();
-        for (int i = 0; i < districts.size(); i++) {
-            countCities.put(districts.get(i), counts.get(i));
-        }
-
-        for (Map.Entry <String, Integer> entry: countCities.entrySet()) {
-            System.out.println(entry.getKey()+ " - " + entry.getValue());
+        for (Map.Entry <String, List <City>> entry: countCities.entrySet()) {
+            System.out.println(entry.getKey()+ " - " + entry.getValue().size());
         }
     }
 
